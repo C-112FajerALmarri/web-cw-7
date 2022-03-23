35932c9b0137647f9771be75c43eca4773358a0a
C-112 Fajer Almarri <70756233+C-112FajerALmarri@users.noreply.github.com>
2022-03-23 18:31:43 +0300

    done

--- Random/script.js
+++ Random/script.js
@@ -1,3 +1,17 @@
 // project أنشئ كائن
+const project = {
+    type :"tech",
+    users : 10000,
+    biography(){
 
+        return"This is my project"
+    }
+}
 // randomProperty أنشئ دالة
+function randomProperty(obj){
+const keys = Object.keys(obj);
+const random = math.floor(Math.random()* keys.length);
+console.log(keys[random]);
+console.log(obj[`${keys[random]}`]);
+}
+randomProperty(project);
